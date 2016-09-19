91e0013fc517cb6ec5a2e8304ba373da8697aa7c
David Edwards <drew.edwards50@gmail.com>
2016-09-19 18:58:16 +0000

    new hello world app

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+var util = require('util');
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+
+app.get('/', function(req, res) {
+    res.send('hello world');
+})
+
+
+
+var server = http.listen(process.env.PORT, function() {
+    util.log('Listening on port: ' + process.env.PORT);
+});
