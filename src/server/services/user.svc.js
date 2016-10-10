95e35c7bdb2fbc39acbb075dcbaf975a70a913e0
David Edwards <drew.edwards50@gmail.com>
2016-10-10 14:29:15 -0400

    I don't remember what I did

---
+++ src/server/services/user.svc.js
@@ -0,0 +1,16 @@
+/**
+ * This service handles user registration and user login functions
+ *
+ * @author David Edwards
+ * @version 0.0.1
+ * @since 10/3/2016
+ */
+
+
+function register(user) {
+
+}
+
+function login(user) {
+
+}
