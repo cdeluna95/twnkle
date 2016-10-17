7c5737bfa40ddfcc7a886d72406b568c15e04b96
ashutosh003 <asharma2@ggc.edu>
2016-10-17 19:05:54 +0000

    added a file to create tables

--- db/db.sql
+++ db/db.sql
@@ -1,14 +1,40 @@
-CREATE TABLE users(
-    userId int AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(30) NOT NULL,
-    lastName VARCHAR(30) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+CREATE TABLE users
+(
+    userId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(20) NOT NULL,
+    lastName VARCHAR(20) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    hashedPassword VARCHAR(256) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     dob DATE NOT NULL,
-    username VARCHAR(30) NOT NULL UNIQUE,
-    password VARCHAR(20) NOT NULL,
     gender VARCHAR(20) NOT NULL,
-    preference ENUM('Male', 'Female', 'Bi'),
-    active TINYINT(1) DEFAULT 1,
-    lastSignin DATE
+    preference ENUM('MALE', 'FEMALE', 'BI') NOT NULL,
+    active TINYINT(4) DEFAULT '1',
+    lastActive DATETIME
 );
 
+CREATE TABLE match
+(
+    matchID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
+    FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
+    matchStatus TINYINT(2) DEFAULT '1'
+)
+
+CREATE TABLE message
+(
+    messageID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (matchID) REFERENCES match(matchID) ON DELETE CASCADE,
+    message VARCHAR(100) NOT NULL,
+    messagedDate DATETIME
+)
+
+CREATE TABLE userPicture
+(
+    pictureID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
+    pictureSource VARCHAR(256) NOT NULL,
+    
+)
+CREATE UNIQUE INDEX users_email_uindex ON users (email);
+CREATE UNIQUE INDEX users_username_uindex ON users (username);
