37c719f805535e908b0fe2119b1f0741d6c04d33
ashutosh003 <asharma2@ggc.edu>
2016-10-24 18:24:14 +0000

    added file that generates 4 tables

--- db/db.sql
+++ db/db.sql
@@ -1,6 +1,11 @@
+drop table userPicture;
+drop table message;
 drop table matches;
 drop table users;
 
+/*
+ This file are to create 4 tables which are users, matches, messages and user Picutures
+*/
 CREATE TABLE users
 (
     userId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -26,8 +31,8 @@ CREATE TABLE matches
         REFERENCES users(userId)
         ON DELETE NO ACTION,
     FOREIGN KEY (userId2) 
-    REFERENCES users(userId)
-    ON DELETE NO ACTION
+        REFERENCES users(userId)
+        ON DELETE NO ACTION
 );
 
 CREATE TABLE message
@@ -35,50 +40,18 @@ CREATE TABLE message
     messageID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     matchId int(11) NOT NULL,
     message VARCHAR(100) NOT NULL,
-    FOREIGN KEY (userId) 
-        REFERENCES users(userId)
+   FOREIGN KEY (matchId) 
+        REFERENCES matches(matchId)
         ON DELETE NO ACTION
-);
-
-/*
-CREATE TABLE users
-(
-    userId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    firstName VARCHAR(20) NOT NULL,
-    lastName VARCHAR(20) NOT NULL,
-    username VARCHAR(20) NOT NULL,
-    hashedPassword VARCHAR(256) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    dob DATE NOT NULL,
-    gender VARCHAR(20) NOT NULL,
-    preference ENUM('MALE', 'FEMALE', 'BI') NOT NULL,
-    active TINYINT(4) DEFAULT '1',
-    lastActive DATETIME
-);
-
-CREATE TABLE matches
-(
-    matchID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY (userId1) REFERENCES users(userId) ON DELETE CASCADE,
-    FOREIGN KEY (userId2) REFERENCES users(userId) ON DELETE CASCADE,
-    matchStatus TINYINT(2) DEFAULT '1'
-);
-
-CREATE TABLE message
-(
-    messageID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY (matchID) REFERENCES match(matchID) ON DELETE CASCADE,
-    message VARCHAR(100) NOT NULL,
-    messagedDate DATETIME
 );
 
 CREATE TABLE userPicture
 (
     pictureID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
-    pictureSource VARCHAR(256) NOT NULL,
+    userId int(11) NOT NULL,
+    FOREIGN KEY (userId) 
+        REFERENCES users(userId)
+        ON DELETE NO ACTION,
+    pictureSource VARCHAR(100) NOT NULL
     
 );
-CREATE UNIQUE INDEX users_email_uindex ON users (email);
-CREATE UNIQUE INDEX users_username_uindex ON users (username);
-*/
