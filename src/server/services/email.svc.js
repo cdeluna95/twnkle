fb6d22ff55432446126dbbcc28a6a033544965d3
David Edwards <dedward3@ggc.edu>
2016-11-16 13:08:48 -0500

    added an emailer service to send verification emails

---
+++ src/server/services/email.svc.js
@@ -0,0 +1,32 @@
+var nodemailer = require('nodemailer');
+
+var getTransport = function() {
+    return nodemailer.createTransport('smtps://dnrtwinkle@gmail.com:Dr.TacksooIm@smtp.gmail.com');
+};
+
+var sendMail = function(transporter, mailOptions) {
+    transporter.sendMail(mailOptions, function(err, info) {
+
+    })
+};
+
+var sendVerificationEmail = function(token, cb) {
+    var transport = getTransport();
+
+    var mailOptions = {
+        from: '',
+        to: '',
+        subject: 'Verify Your Twnkle Account',
+        test: 'Please click the link below'
+    };
+
+    sendMail(transport, mailOptions, function(err, info) {
+        if(err) {
+
+        }
+    });
+};
+
+module.exports = {
+    sendVerificationEmail: sendVerificationEmail
+};
